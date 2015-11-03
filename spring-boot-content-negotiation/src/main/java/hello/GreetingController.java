8c8a43d588ae6d6c507fb2fdad9076fd675ccf05
Enrique Ruiz <eruiz@disid.com>
2015-11-03 12:44:19 +0100

    Spring 4 and Spring Boot Content-Negotiation proof of concept

---
+++ spring-boot-content-negotiation/src/main/java/hello/GreetingController.java
@@ -0,0 +1,34 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.POST)
+    public Greeting greeting(@RequestBody Greeting g) {
+        return new Greeting(counter.incrementAndGet(),
+                            g.getContent());
+    }
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.PUT)
+    public Greeting salutation(@RequestBody Salutation s) {
+        return new Greeting(counter.incrementAndGet(),
+                            s.getContent());
+    }
+}
---
+++ spring-boot-content-negotiation/src/main/java/hello/Salutation.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class Salutation {
+
+    private String content;
+
+    public Salutation() {}
+
+    public Salutation(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
