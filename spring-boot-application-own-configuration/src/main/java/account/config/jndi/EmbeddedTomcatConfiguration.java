fe8acf362431c821c10e38e45415c03cff47bc92
Enrique Ruiz <eruiz@disid.com>
2015-12-16 12:34:30 +0100

    Proof of concept that shows the use of the Spring Boot configuration levels

---
+++ spring-boot-application-own-configuration/src/main/java/account/config/jndi/EmbeddedTomcatConfiguration.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2015 DISID CORPORATION S.L.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see `<http://www.gnu.org/licenses/>`.
+ */
+package account.config.jndi;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.ContextResource;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmbeddedTomcatConfiguration {
+
+    @Bean
+    public TomcatEmbeddedServletContainerFactory tomcatFactory() {
+        return new TomcatEmbeddedServletContainerFactory() {
+
+            @Override
+            protected TomcatEmbeddedServletContainer getTomcatEmbeddedServletContainer(
+                    Tomcat tomcat) {
+                tomcat.enableNaming();
+                return super.getTomcatEmbeddedServletContainer(tomcat);
+            }
+
+            @Override
+            protected void postProcessContext(Context context) {
+                ContextResource resource = new ContextResource();
+                resource.setName("DUMMY_TYPE");
+                resource.setProperty("DUMMY_TYPE", "type-in-JNDI-prop");
+                resource.setProperty("type-in-JNDI-prop", "type-in-JNDI-prop");
+                resource.setType(String.class.getName());
+
+                context.getNamingResources().addResource(resource);
+            }
+        };
+    }
+}
