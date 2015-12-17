23db6946fcbff15b7b23fd3d2a02463bfe448bb0
jcagarcia <jugade92@gmail.com>
2015-12-17 15:22:48 +0100

    Configuring application to be deployed on embedded tomcat server or in JEE server without apply any changes

--- spring-boot-application-own-configuration/src/main/java/account/config/ApplicationOwnConfigEnvironmentPostProcessor.java
+++ spring-boot-application-own-configuration/src/main/java/account/config/ApplicationOwnConfigEnvironmentPostProcessor.java
@@ -20,8 +20,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.config.RandomValuePropertySource;
 import org.springframework.boot.env.EnvironmentPostProcessor;
