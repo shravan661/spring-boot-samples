dd8596b49e061f01c727f9ca01c4135bcfc0d850
Enrique Ruiz <eruiz@disid.com>
2016-01-27 15:14:37 +0100

    Use Spring Boot Legacy to deploy in WL 12.2.1

--- spring-boot-weblogic/src/main/java/account/Application.java
+++ spring-boot-weblogic/src/main/java/account/Application.java
@@ -2,17 +2,28 @@ package account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.web.WebApplicationInitializer;
 
+/**
+ * = Application
+ *
+ * Project version: {projectVersion}.
+ *
+ * Sample Java application in project *{projectName}* 
+ * to show Asciidoclet as replacement for the
+ * default Javadoclet.
+ *
+ * include::README.adoc[]
+ *
+ * We can apply Asciidoc syntax in our Javadoclet comments, like:
+ *
+ *  - `code`
+ *  - **bold**
+ *  - _italics_
+ *
+ * @author eruiz
+ */
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer implements WebApplicationInitializer {
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-      return application.sources(Application.class);
-  }
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- spring-boot-weblogic/src/main/java/account/service/AccountServiceImpl.java
+++ spring-boot-weblogic/src/main/java/account/service/AccountServiceImpl.java
@@ -3,20 +3,45 @@ package account.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import account.domain.Account;
 
+/**
+ * = Javadoc in Asciidoc
+ * 
+ * Sample comments in `Asciidoctor`.
+ * 
+ * @author eruiz
+ */
 @Service
 public class AccountServiceImpl implements AccountService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AccountServiceImpl.class);
 
     @Value("${dummy.type}")
     private String dummyType;
 
     /**
      * {@inheritDoc}
-     * <p/>
+     * 
+     * .Asciidoclet demo
+     * [source,java]
+     * --
+     * public class Asciidoclet extends Doclet {
+     *     private final Asciidoctor asciidoctor = Asciidoctor.Factory.create();
+     *
+     *     @SuppressWarnings("UnusedDeclaration")
+     *     public static boolean start(RootDoc rootDoc) {
+     *         new Asciidoclet().render(rootDoc);
+     *         return Standard.start(rootDoc);
+     *     }
+     * }
+     * --
+     * 
      * Dummy method for testing purposes.
      * 
      * @param number The account number. Set 0000 to get an {@link AccountNotFoundException} 
@@ -27,8 +52,12 @@ public class AccountServiceImpl implements AccountService {
         if(number.equals("0000")) {
             throw new AccountNotFoundException("0000");
         }
-        Account account = new Account(0, number);
+        Account account = new Account(1, number);
         account.setType(getDummyType());
+        account.setCreditCardNumber("1111 1111 1111 1111");
+
+        LOG.info("New Account: ".concat(account.toString()));
+
         return account;
     }
 
